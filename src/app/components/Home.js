fd6af65f76bfbf3ec30a8b75210c33b1ad3eb808
jttwnsnd <jt.a.townsend@gmail.com>
2016-11-17 13:19:45 -0500

    props

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -5,6 +5,14 @@ export class Home extends React.Component {
     return (
       <div>
         <p>In a new Component!</p>
+        <p>Your name is {this.props.name}, your age is {this.props.age}</p>
+        <p>User Object => Name: {this.props.user.name}</p>
+        <div>
+          <h4>Hobbies</h4>
+          <ul>
+            {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
+          </ul>
+        </div>
       </div>
     );
   }
